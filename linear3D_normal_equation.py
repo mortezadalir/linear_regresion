c4dbb67f8d09c5dcf01d410833369c5ee040eae9
morteza dalirian <morteza.daliriyan@gmail.com>
2020-08-27 07:50:22 +0430

    3D linear regresion(normal eqaution

--- linear3D_normal_equation.py
+++ linear3D_normal_equation.py
@@ -16,8 +16,4 @@ X=np.matrix.transpose(X_T)
 y_t=np.matrix.transpose(XX[:,2])
 XY1=np.dot(X_T,y_t)
 teta=np.dot(np.linalg.inv(np.dot(X_T,X)),XY1)
-x1=np.linspace(20,120,20)
-y1=teta[1]*x1+teta[0]
-colors1=('red')
-plt.plot(x1,y1,c=colors1)
-plt.show()
+
